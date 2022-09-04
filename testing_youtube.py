7b00db59fba2d41405e0bbfe701680730aff6275
vinay krishna <vinay221097@gmail.com>
2022-09-04 12:34:36 +0200

    six

--- testing_youtube.py
+++ testing_youtube.py
@@ -34,10 +34,10 @@ path=os.environ.get('CHROME_PATH')
 p=subprocess.Popen(path + " --remote-debugging-port=8989 --user-data-dir={} --no-first-run --no-default-browser-check ".format(os.getcwd()+'/chrome/'),shell=True,stdout=subprocess.PIPE)
 out, err = p.communicate()
 print("abc",out)
-print(os.listdir(os.getcwd()+'/chrome/'))
+print(os.listdir(os.getcwd()+'/chrome/BrowserMetrics'))
 
 # print(os.stat(path+'/chrome'))
-path=os.getcwd()+'/chrome/chromedriver'
+path=os.getcwd()+'/chrome/.org.chromium.Chromium.zLGDPO'
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
