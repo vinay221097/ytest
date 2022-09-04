11e9c1c4f824c14f5b1681259d355f31e1f1047e
vinay krishna <vinay221097@gmail.com>
2022-09-04 11:09:28 +0200

    six

--- test2.py
+++ test2.py
@@ -12,7 +12,7 @@ class MyBrowser(QWebPage):
     
     def userAgentForUrl(self, url):
         ''' Returns a User Agent that will be seen by the website. '''
-return "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
+        return "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36"
 
 class Browser(QWebView):
     def __init__(self):
