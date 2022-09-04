f4fff0e2e81675891f24f66ab14df089e4bd6b80
vinay krishna <vinay221097@gmail.com>
2022-09-04 12:11:49 +0200

    six

--- testing_youtube.py
+++ testing_youtube.py
@@ -30,20 +30,23 @@ options = Options()
 # options.add_argument('--headless')
 # path=r"/opt/build/repo/node_modules/chromium/lib/chromium/chrome-linux/chrome"
 
-# path=os.environ.get('CHROME_PATH')
-# p=subprocess.Popen(path + " --remote-debugging-port=9288",shell=True,stdout=subprocess.PIPE)
-# out, err = p.communicate()
-# print(out)
+path=os.environ.get('CHROME_PATH')
+p=subprocess.Popen(path + " --remote-debugging-port=8989 --user-data-dir={} --no-first-run --no-default-browser-check ".format(os.getcwd()+'/chrome/'),shell=True,stdout=subprocess.PIPE)
+out, err = p.communicate()
+print(out)
 # print(os.listdir(path))
 
 # print(os.stat(path+'/chrome'))
-path=r'chromedriver'
-driver = webdriver.Chrome(executable_path=path,chrome_options=options)
-
-
+path=os.getcwd()+'/chrome/chromedriver'
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
+options.add_experimental_option("debuggerAddress", "127.0.0.1:8989")
+
+driver = webdriver.Chrome(executable_path=path,chrome_options=options)
+
+
+
 # options.add_argument('--disable-dev-shm-usage')
 # service=Service(ChromeDriverManager(path = r"Drivers").install())
 # path=os.getcwd()+'/Drivers/.wdm/drivers/chromedriver'
