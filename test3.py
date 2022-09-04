9c5da4485a2cbf3f914c7f8cf0909ef59bdc1af8
vinay krishna <vinay221097@gmail.com>
2022-09-04 12:47:48 +0200

    six

--- test3.py
+++ test3.py
@@ -1,17 +1,24 @@
-import spynner
-import pyquery
+from selenium import webdriver
+from selenium.webdriver.remote.webdriver import WebDriver
 
-browser = spynner.Browser(debug_level=spynner.INFO)
-browser.create_webview()
-browser.show()
-browser.load("http://www.wordreference.com")
-browser.load_jquery(True)
-browser.choose("input[name=lr=lang_es]")
-browser.click("input[name=enit]")
-browser.click("a[class=l]:first")
-d = pyquery.PyQuery(browser.html)
-d.make_links_absolute(base_url=browser.url)
-href = d('a:last').attr('href')
-print(href)
-print(len(browser.download(href)))
-browser.browse()
+# executor_url = driver.command_executor._url
+# session_id = driver.session_id
+
+def attach_to_session(executor_url, session_id):
+    original_execute = WebDriver.execute
+    def new_command_execute(self, command, params=None):
+        if command == "newSession":
+            # Mock the response
+            return {'success': 0, 'value': None, 'sessionId': session_id}
+        else:
+            return original_execute(self, command, params)
+    # Patch the function before creating the driver object
+    WebDriver.execute = new_command_execute
+    driver = webdriver.Remote(command_executor=executor_url, desired_capabilities={})
+    driver.session_id = session_id
+    # Replace the patched function with original function
+    WebDriver.execute = original_execute
+    return driver
+
+bro = attach_to_session('http://127.0.0.1:64092', '8de24f3bfbec01ba0d82a7946df1d1c3')
+bro.get('http://ya.ru/')
--- testing_youtube.py
+++ testing_youtube.py
@@ -30,18 +30,20 @@ options = Options()
 # options.add_argument('--headless')
 # path=r"/opt/build/repo/node_modules/chromium/lib/chromium/chrome-linux/chrome"
 
-path=os.environ.get('CHROME_PATH')
-p=subprocess.Popen(path + " --remote-debugging-port=8989 --user-data-dir={} --no-first-run --no-default-browser-check ".format(os.getcwd()+'/chrome/'),shell=True,stdout=subprocess.PIPE)
-out, err = p.communicate()
-print("abc",out)
-print(os.listdir(os.getcwd()+'/chrome/BrowserMetrics'))
+# path=os.environ.get('CHROME_PATH')
+# p=subprocess.Popen(path + " --remote-debugging-port=8989 --user-data-dir={} --no-first-run --no-default-browser-check ".format(os.getcwd()+'/chrome/'),shell=True,stdout=subprocess.PIPE)
+# out, err = p.communicate()
+# print("abc",out)
+# print(os.listdir(os.getcwd()+'/chrome/BrowserMetrics'))
 
 # print(os.stat(path+'/chrome'))
+
 path=os.getcwd()+'/chrome/.org.chromium.Chromium.zLGDPO'
+path=r'chrome/chromedriver'
 options = Options()
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
-options.add_experimental_option("debuggerAddress", "127.0.0.1:8989")
+# options.add_experimental_option("debuggerAddress", "127.0.0.1:8989")
 
 driver = webdriver.Chrome(executable_path=path,chrome_options=options)
 driver.get('https://google.com')
